0fbaaf8da30acac3eb85931d4936732524f55eac
Matheus Souza <mfbsouza.it@gmail.com>
2023-08-08 23:02:36 -0300

    implemented basic node functions needed to receive data from the gateway (#11)
    * finished coding the functions needed to recive data from gateway
    * wrote test cases for the functions in node to recive data from the gw

--- src/snpes_node.h
+++ src/snpes_node.h
@@ -26,7 +26,7 @@ SnpesStatus_t snpes_scan(uint8_t *gateway_uid);
 /**
  * @brief connect to a gateway by a given UID
  *
- * @param *gateway_uid: pointer to store the found gateway uid
+ * @param gateway_uid: unique id of the gateway
  * @retval: SNPES_OK or SNPES_ERROR
  */
 SnpesStatus_t snpes_connect(uint8_t gateway_uid);
@@ -42,9 +42,28 @@ SnpesStatus_t snpes_connect(uint8_t gateway_uid);
 SnpesStatus_t snpes_send(uint8_t dest_uid, const void *src, uint8_t size);
 
 /**
- * @brief calls all the gateway modules
+ * @brief check if the gateway is sending some request to the client
  * should be used in a loop function, like a pooling
+ *
+ * @param gateway_uid: unique id of the gateway
  */
-//void snpes_compute(void);
+void snpes_node_sync(uint8_t gateway_uid);
+
+/**
+ * @brief checks how many bytes are available to read from the gateway
+ *
+ * @retval: returns the amount of bytes available to read
+ */
+uint8_t snpes_node_available_data(void);
+
+/**
+ * @brief copies the available data to the user destination
+ *
+ * @param *dest: pointer to the destination where the data will be copied
+ * @param amount: amount of bytes to be copied
+ *
+ * @retval: returns the amount of bytes copied
+ */
+uint8_t snpes_node_read(void *dest, uint8_t amount);
 
 #endif /* __SNPES_H__ */
